b4184f4245974a9241103673ad0db8b10732ef8f
Cristobal Dominguez <cristobald@gmail.com>
2023-02-14 13:01:38 +0100

    Improves Error Formatted Return

--- middlewares/error_middleware.js
+++ middlewares/error_middleware.js
@@ -2,9 +2,11 @@ function errorMiddleware(err, req, res, next) {
   if (err.is_an_error) return res.status(err.status).json(err.toJson())
 
   res.status(500).json({
-    status: 500,
-    name: 'UnknownError',
-    message: 'Unknown Error'
+    error: {
+      status: 500,
+      name: 'UnknownError',
+      message: 'Unknown Error'
+    }
   })
 }
 
